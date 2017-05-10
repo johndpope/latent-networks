729598c98c2d2adee94e6d95102364451eb142bf
AndroidCloud <anirudhgoyal9119@gmail.com>
2017-05-10 00:57:39 -0400

    something wrong is going on

---
+++ train_lstm_ptb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from lm_lstm_ptb import train
+
+
+def main(job_id, params):
+    print params
+    validerr = train(
+        saveto=params['model'][0],
+        reload_=params['reload'][0],
+        dim_word=params['dim_word'][0],
+        dim=params['dim'][0],
+        n_words=params['n-words'][0],
+        decay_c=params['decay-c'][0],
+        lrate=params['learning-rate'][0],
+        optimizer=params['optimizer'][0],
+        maxlen=30,
+        batch_size=32,
+        valid_batch_size=32,
+        validFreq=5000,
+        dispFreq=10,
+        saveFreq=1000,
+        sampleFreq=1000,
+        dataset='/data/lisatmp4/anirudhg/ptb/ptb_train_50w.txt',
+        valid_dataset='/data/lisatmp4/anirudhg/ptb/ptb_valid.txt',
+        dictionary='/data/lisatmp4/anirudhg/ptb/ptb_dict_word.pkl',
+        use_dropout=params['use-dropout'][0])
+    return validerr
+
+if __name__ == '__main__':
+    main(0, {
+        'model': ['/data/lisatmp4/anirudhg/latent_autoregressive/models/model_len30_0.05_kl.npz'],
+        'dim_word': [512],
+        'dim': [620],
+        'n-words': [30000],
+        'optimizer': ['adam'],
+        'decay-c': [0.],
+        'use-dropout': [False],
+        'learning-rate': [0.0001],
+        'reload': [False]})
