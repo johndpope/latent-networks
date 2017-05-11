daa566ec51b549ba823383f703d37d6c0b431b83
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2017-05-11 08:56:52 -0400

    Fix evaluation

--- lm_lstm_blizzard.py
+++ lm_lstm_blizzard.py
@@ -197,77 +197,6 @@ def concatenate(tensor_list, axis=0):
     return out
 
 
-class TimitData():
-    def __init__(self, fn, batch_size):
-        import numpy as np
-        data = np.load(fn)
-
-        ####
-        # IMPORTANT: u_train is the input and x_train is the target.
-        ##
-        u_train, x_train = data['u_train'], data['x_train']
-        u_valid, x_valid = data['u_valid'], data['x_valid']
-        (u_test, x_test, mask_test) = data['u_test'],  data['x_test'], data['mask_test']
-
-        # assert u_test.shape[0] == 1680
-        # assert x_test.shape[0] == 1680
-        # assert mask_test.shape[0] == 1680
-
-        self.u_train = u_train
-        self.x_train = x_train
-        self.u_valid = u_valid
-        self.x_valid = x_valid
-
-        # make multiple of batchsize
-        n_test_padded = ((u_test.shape[0] // batch_size) + 1)*batch_size
-        assert n_test_padded > u_test.shape[0]
-        pad = n_test_padded - u_test.shape[0]
-        u_test = np.pad(u_test, ((0, pad), (0, 0), (0, 0)), mode='constant')
-        x_test = np.pad(x_test, ((0, pad), (0, 0), (0, 0)), mode='constant')
-        mask_test = np.pad(mask_test, ((0, pad), (0, 0)), mode='constant')
-        self.u_test = u_test
-        self.x_test = x_test
-        self.mask_test = mask_test
-
-        self.n_train = u_train.shape[0]
-        self.n_valid = u_valid.shape[0]
-        self.n_test = u_test.shape[0]
-        self.batch_size = batch_size
-
-        print("TRAINING SAMPLES LOADED", self.u_train.shape)
-        print("TEST SAMPLES LOADED", self.u_test.shape)
-        print("VALID SAMPLES LOADED", self.u_valid.shape)
-        print("TEST AVG LEN        ", np.mean(self.mask_test.sum(axis=1)) * 200)
-        # test that x and u are correctly shifted
-        assert np.sum(self.u_train[:, 1:] - self.x_train[:, :-1]) == 0.0
-        assert np.sum(self.u_valid[:, 1:] - self.x_valid[:, :-1]) == 0.0
-        for row in range(self.u_test.shape[0]):
-            l = int(self.mask_test[row].sum())
-            if l > 0:  # if l is zero the sequence is fully padded.
-                assert np.sum(self.u_test[row, 1:l] -
-                              self.x_test[row, :l-1]) == 0.0, row
-
-    def _iter_data(self, u, x, mask=None):
-        # IMPORTANT: In SRNN (where the data come from) u refers to the input whereas x, to the target.
-        indices = range(len(u))
-        for idx in chunk(indices, n=self.batch_size):
-            u_batch, x_batch = u[idx], x[idx]
-            if mask is None:
-                mask_batch = np.ones((x_batch.shape[0], x_batch.shape[1]), dtype='float32')
-            else:
-                mask_batch = mask[idx]
-            yield u_batch, x_batch, mask_batch
-
-    def get_train_batch(self):
-        return iter(self._iter_data(self.u_train, self.x_train))
-
-    def get_valid_batch(self):
-        return iter(self._iter_data(self.u_valid, self.x_valid))
-
-    def get_test_batch(self):
-        return iter(self._iter_data(self.u_test, self.x_test, mask=self.mask_test))
-
-
 # feedforward layer: affine transformation + point-wise nonlinearity
 def param_init_fflayer(options, params, prefix='ff', nin=None, nout=None,
                        ortho=True):
@@ -646,12 +575,10 @@ def pred_probs(f_log_probs, options, data, source='valid'):
     rvals = []
     n_done = 0
 
-    next_batch = (lambda: data.get_valid_batch()) \
-        if source == 'valid' else (lambda: data.get_test_batch())
-    for x, y, x_mask in next_batch():
-        x = x.transpose(1, 0, 2)
-        y = y.transpose(1, 0, 2)
-        x_mask = x_mask.transpose(1, 0)
+    for data_ in data:
+        x = data_[0][0]
+        y = data_[1][0]
+        x_mask = np.ones((x.shape[0], x.shape[1]), dtype='float32')
         n_done += x.shape[1]
 
         zmuv = numpy.random.normal(loc=0.0, scale=1.0, size=(
